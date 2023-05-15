9521d540575034e9d8303a3d5e5568cf0ed72fd5
Iron-E <code.iron.e@gmail.com>
2023-05-15 17:36:36 -0400

    fix(Coordinate): use AsRef for inner arrays
    This fixes an issue where no 2D array I could make would be accepted by
    the `get()` method.

--- src/map/coordinate.rs
+++ src/map/coordinate.rs
@@ -1,6 +1,6 @@
 /// # Summary
 ///
-/// A __(__`y`__,__ `x`__)__ tuple which refers to coordinates in a two-dimensional array.
+/// A __(__`x`__,__ `y`__)__ tuple which refers to coordinates in a two-dimensional array.
 #[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Coordinate(pub usize, pub usize);
 
@@ -9,16 +9,37 @@ impl Coordinate
 	/// # Summary
 	///
 	/// Retrieve the `T` value stored at the [`Coordinate`] in array.
-	pub fn get<T>(&self, array: &[&[T]]) -> Option<T> where T : Copy
+	pub fn get<T>(&self, array: &[impl AsRef<[T]>]) -> Option<T> where
+		T : Copy,
 	{
-		if let Some(inner) = array.get(self.0)
+		if let Some(inner) = array.get(self.1)
 		{
-			if let Some(value) = inner.get(self.1)
+			if let Some(value) = inner.as_ref().get(self.0)
 			{
 				return Some(*value);
 			}
 		}
 
 		None
+	}
+}
+
+#[cfg(test)]
+mod tests
+{
+	use super::Coordinate;
+
+	#[test]
+	fn test_get()
+	{
+		let array = vec![
+			vec![1,  2,  3,  4,  5],
+			vec![6,  7,  8,  9,  10],
+			vec![11, 12, 13, 14, 15],
+			vec![16, 17, 18, 19, 20],
+			vec![21, 22, 23, 24, 25],
+		];
+
+		assert_eq!(Coordinate(2, 2).get(&array), Some(13));
 	}
 }

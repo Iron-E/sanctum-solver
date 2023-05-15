d286e9b030e909b4b4cd02d32179ce4ab5f525a3
Iron-E <code.iron.e@gmail.com>
2023-05-15 17:36:55 -0400

    feat(App): write `run` method

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 mod error;
 
 use {
-	crate::map::Map,
+	crate::map::{Build, Map, tileset::Tileset},
 	error::Result,
 	std::{fs, path::PathBuf},
 	structopt::StructOpt,
@@ -10,14 +10,17 @@ use {
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, StructOpt)]
 #[structopt(
 	name = "sanctum_solver",
-	about = "A tool to find the most optimal layout for a Sanctum map."
+	about = "A tool to find the most optimal layout for a Sanctum map"
 )]
 pub struct App {
-	#[structopt(help = "The maximum number of blocks to place.", long, short)]
+	#[structopt(help = "A JSON file containing the map layout")]
+	map_json: PathBuf,
+
+	#[structopt(help = "The maximum number of blocks to place", long, short)]
 	blocks: Option<usize>,
 
-	#[structopt(help = "A JSON file containing the map layout.")]
-	map_json: PathBuf,
+	#[structopt(help="Where to save the output. If not specified, goes to `stdout`", long, short)]
+	output: Option<PathBuf>,
 }
 
 impl App {
@@ -25,8 +28,21 @@ impl App {
 	///
 	/// Run the application and parse its provided arguments / flags.
 	pub fn run(self) -> Result<()> {
-		let map: Map = serde_json::from_slice(&fs::read(self.map_json)?)?;
+		let mut map: Map = serde_json::from_slice(&fs::read(self.map_json)?)?;
+		let mut tileset = Tileset::new(map.grid);
 
-		todo!()
+		let build = Build::from_entrances_to_any_core(&tileset, self.blocks);
+		build.apply_to(&mut tileset.grid);
+
+		map.grid = tileset.grid;
+
+		let map_json = serde_json::to_string_pretty(&map)?;
+		if let Some(output) = self.output {
+			fs::write(output, map_json)?;
+		} else {
+			println!("{}", map_json);
+		}
+
+		Ok(())
 	}
 }

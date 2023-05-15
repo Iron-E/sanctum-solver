6ffda5ec185005eb0cd2bb1648ee385d54c50f9a
Iron-E <code.iron.e@gmail.com>
2023-05-15 17:36:43 -0400

    feat(Coordiante): impl distance_from
    This will tell us how far apart two coordinates are.
    
    TODO: refactor traversal algorithms to work out how many tiles they have
    visited so far so that this method is not necessary.

--- src/map/coordinate.rs
+++ src/map/coordinate.rs
@@ -7,6 +7,42 @@ pub struct Coordinate(pub usize, pub usize);
 impl Coordinate {
 	/// # Summary
 	///
+	/// Get the distance between two [`Coordinate`]s.
+	///
+	/// # Remarks
+	///
+	/// This is a very naive implementation of distance counting and should only be done _after_ it
+	/// is ensured that two coordinates have a navigable path to one another.
+	///
+	/// For example, it does not account for if one point on the grid is
+	/// [impassable](super::Tile::is_passable).
+	///
+	/// # Example
+	///
+	/// ```rust
+	/// use sanctum_solver::map::Coordinate;
+	///
+	/// let a = Coordinate(0, 4);
+	/// let b = Coordinate(2, 3);
+	/// let c = Coordinate(0, 1);
+	///
+	/// // _ _ _ _ _
+	/// // c _ _ _ _
+	/// // _ _ _ _ _
+	/// // _ _ b _ _
+	/// // a _ _ _ _
+	///
+	/// assert_eq!(a.distance_from(b), 3);
+	/// assert_eq!(a.distance_from(b), b.distance_from(a));
+	/// assert_eq!(a.distance_from(b), a.distance_from(c));
+	/// ```
+	pub fn distance_from(&self, other: Coordinate) -> usize {
+		((other.0 as i128 - self.0 as i128).abs() + (other.1 as i128 - self.1 as i128).abs())
+			as usize
+	}
+
+	/// # Summary
+	///
 	/// Retrieve the `T` value stored at the [`Coordinate`] in array.
 	pub fn get_from<T>(&self, array: &[impl AsRef<[T]>]) -> Option<T>
 	where

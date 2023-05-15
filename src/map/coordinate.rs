e0d96d910fcf220cf8e3a0e20eb630834cf42777
Iron-E <code.iron.e@gmail.com>
2023-05-15 17:36:40 -0400

    ref(Coordinate): use a constant

--- src/map/coordinate.rs
+++ src/map/coordinate.rs
@@ -26,18 +26,18 @@ impl Coordinate {
 mod tests {
 	use super::Coordinate;
 
+	const ARRAY: [[usize; 5]; 5] = [
+		[1, 2, 3, 4, 5],
+		[6, 7, 8, 9, 10],
+		[11, 12, 13, 14, 15],
+		[16, 17, 18, 19, 20],
+		[21, 22, 23, 24, 25],
+	];
+
 	#[test]
 	fn test_get_from() {
-		let array = vec![
-			vec![1, 2, 3, 4, 5],
-			vec![6, 7, 8, 9, 10],
-			vec![11, 12, 13, 14, 15],
-			vec![16, 17, 18, 19, 20],
-			vec![21, 22, 23, 24, 25],
-		];
-
-		assert_eq!(Coordinate(2,2).get_from(&array), Some(13));
-		assert_eq!(Coordinate(0,1).get_from(&array), Some(6));
-		assert_eq!(Coordinate(100,1).get_from(&array), None);
+		assert_eq!(Coordinate(2, 2).get_from(&ARRAY), Some(13));
+		assert_eq!(Coordinate(0, 1).get_from(&ARRAY), Some(6));
+		assert_eq!(Coordinate(100, 1).get_from(&ARRAY), None);
 	}
 }
